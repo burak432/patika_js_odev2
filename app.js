506de14896b365d4138aa5cc9433da2e0a420afc
Burak Atilla <burak.atilla@gmail.com>
2022-01-18 06:27:27 +0300

    1 | patika js ödev 2. ödev tamamlandı

---
+++ app.js
@@ -0,0 +1,63 @@
+const toast1 = document.querySelector("#liveToast1");
+const toast2 = document.querySelector("#liveToast2");
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
+
+function newElement() {
+  let li = document.createElement("li");
+  var inputValue = document.querySelector("#task").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+
+  if (inputValue.trim() === "") {
+    let toast = new bootstrap.Toast(toast1);
+    toast.show();
+  } else {
+    document.querySelector("#list").appendChild(li);
+    let toast = new bootstrap.Toast(toast2);
+    toast.show();
+  }
+
+  document.getElementById("task").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
